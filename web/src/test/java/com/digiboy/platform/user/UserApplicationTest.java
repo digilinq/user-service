8af644a86d214f2528f8c1b23a7e61cae1ca2fef
Hossein Mohammadi <hmohammadi@mobiquityinc.com>
2022-11-19 18:35:26 +0100

    User microservice

---
+++ web/src/test/java/com/digiboy/platform/user/UserApplicationTest.java
@@ -0,0 +1,12 @@
+package com.digiboy.platform.user;
+
+import com.digiboy.platform.user.web.integration.IntegrationTest;
+import org.junit.jupiter.api.Test;
+
+@IntegrationTest
+class UserApplicationTest {
+
+    @Test
+    void loadContext() {
+    }
+}
